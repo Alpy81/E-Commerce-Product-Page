fea9c629403a1481faf8e4a08b1107be16ac62ec
Alpy81 <alperen.ertas@gmx.de>
2023-12-31 16:55:37 +0100

    update commit

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,48 +2,65 @@
 let currentIndex = 0;
 
 function openLightBox(className) {
- // Update currentIndex based on className
- const classes = ['product-one', 'product-two', 'product-three', 'product-four'];
- currentIndex = classes.indexOf(className);
+  // Update currentIndex based on className
+  const classes = [
+    "product-one",
+    "product-two",
+    "product-three",
+    "product-four",
+  ];
+  currentIndex = classes.indexOf(className);
 
- // Get the main-pic element
- const mainPicElement = document.querySelector('.main-pic');
+  // Get the main-pic element
+  const mainPicElement = document.querySelector(".main-pic");
 
- // Hide all images in the .main-pic div
- Array.from(mainPicElement.children).forEach(img => img.style.display = 'none');
+  // Hide all images in the .main-pic div
+  Array.from(mainPicElement.children).forEach(
+    (img) => (img.style.display = "none")
+  );
 
- // Show the corresponding image in the .main-pic div
- mainPicElement.querySelector('.' + className).style.display = 'block';
+  // Show the corresponding image in the .main-pic div
+  mainPicElement.querySelector("." + className).style.display = "block";
 
- // Show the lightbox
- document.getElementById('lightbox').style.display = 'flex';
+  // Show the lightbox
+  document.getElementById("lightbox").style.display = "flex";
 }
 
 function closeLightBox() {
- // Hide the lightbox
- document.getElementById('lightbox').style.display = 'none';
+  // Hide the lightbox
+  document.getElementById("lightbox").style.display = "none";
 }
 
 function changeImage(offset) {
- // Calculate the new index
- const newIndex = (currentIndex + offset + 4) % 4; // +4 because of modulo operation
+  // Calculate the new index
+  const newIndex = (currentIndex + offset + 4) % 4;
 
- // Hide all images in the .main-pic div
- const mainPicElement = document.querySelector('.main-pic');
- Array.from(mainPicElement.children).forEach(img => img.style.display = 'none');
+  // Hide all images in the .main-pic div
+  const mainPicElement = document.querySelector(".main-pic");
+  Array.from(mainPicElement.children).forEach(
+    (img) => (img.style.display = "none")
+  );
 
- // Show the corresponding image in the .main-pic div
- mainPicElement.children[newIndex].style.display = 'block';
+  // Show the corresponding image in the .main-pic div
+  mainPicElement.children[newIndex].style.display = "block";
 
- // Update currentIndex
- currentIndex = newIndex;
+  // Update currentIndex
+  currentIndex = newIndex;
 }
 
-// Add event listeners to the previous and next icons
-document.getElementById('prev-icon').addEventListener('click', function() {
- changeImage(-1);
-});
+function initializeEventListeners() {
+  const prevIcon = document.getElementById("prev-icon");
+  const nextIcon = document.getElementById("next-icon");
 
-document.getElementById('next-icon').addEventListener('click', function() {
- changeImage(1);
-});
+  prevIcon.addEventListener("click", function (event) {
+    event.stopPropagation();
+    changeImage(-1);
+  });
+
+  nextIcon.addEventListener("click", function (event) {
+    event.stopPropagation();
+    changeImage(1);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initializeEventListeners);
